ace844edd54915ddc0a363f4192d8007c412fd08
Steve Bottelbergs <steve.bottelbergs@continuum.be>
2019-11-20 16:53:33 +0100

    Cache 2

--- src/main/java/messages/GetMessage.java
+++ src/main/java/messages/GetMessage.java
@@ -1,9 +1,7 @@
 package messages;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.Collections;
 
 public class GetMessage {
     public static void main(String[] args) {
@@ -15,12 +13,28 @@ public class GetMessage {
             em = emf.createEntityManager();
             EntityTransaction tx = em.getTransaction();
             tx.begin();
+
             Message message = em.find(Message.class, 1L);
-            System.out.println(message.getText());
-            System.out.println("In cache: " + emf.getCache().contains(Message.class, message.getId()));
-            emf.getCache().evict(Message.class, message.getId());
-            System.out.println("In cache: " + emf.getCache().contains(Message.class, message.getId()));
+            if (message != null) {
+                em.remove(message);
+                tx.commit();
+                emf.getCache().evictAll();
+                tx.begin();
+            }
+            message = new Message(1L, "Hello World!");
+            em.persist(message);
             tx.commit();
+
+            System.out.println("In cache (after persist): " + emf.getCache().contains(Message.class, message.getId()));
+            emf.getCache().evict(Message.class, message.getId());
+            em.detach(message);
+            System.out.println("In cache (after evict/detach): " + emf.getCache().contains(Message.class, message.getId()));
+
+            message = em.find(Message.class, 1L);
+            System.out.println("In cache (after find 2): " + emf.getCache().contains(Message.class, message.getId()));
+
+            emf.getCache().evictAll();
+            System.out.println("In cache (after evict all): " + emf.getCache().contains(Message.class, message.getId()));
         } finally {
             if (em != null) {
                 em.close();
--- src/main/java/messages/TestMessage.java
+++ src/main/java/messages/TestMessage.java
@@ -1,9 +1,6 @@
 package messages;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class TestMessage {
     private static EntityManagerFactory emf;
